8dfbc345fd309f782b9d36934d0458544549be2b
pkopper <philipp.kopper@am-ammersee.de>
2024-01-17 18:18:49 +0100

    add partial prediction

--- R/deeppamm.R
+++ R/deeppamm.R
@@ -316,9 +316,11 @@ deeppamm <- R6::R6Class(
         }
         tabular_terms[[i]] <- list(structured = processed_terms_t,
                                    deep = formulas[[i]]$deep_vars)
-        if (partial_) {
-          if (!is_structured) {
-            X2[[i]][!(names(X2[[i]]) %in% self$tabular_terms[[i]])] <- X2[[i]][!(names(X2[[i]]) %in% self$tabular_terms[[i]])] * 0
+        if (!train) {
+          if (partial_) {
+            if (!is_structured) {
+              X2[[i]][!(names(X2[[i]]) %in% self$tabular_terms[[i]])] <- X2[[i]][!(names(X2[[i]]) %in% self$tabular_terms[[i]])] * 0
+            }
           }
         }
       }
